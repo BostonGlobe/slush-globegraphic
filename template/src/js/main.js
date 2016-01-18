7164ddc5462f36eb0dab0d462ff892b811af3599
Gabriel Florit <gabrielflorit@gmail.com>
2016-01-18 15:18:10 -0500

    Move pym to https

--- template/src/js/main.js
+++ template/src/js/main.js
@@ -1,6 +1,4 @@
-'use strict';
-
-var globeIframe = require('globe-iframe-resizer');
+import globeIframe from 'globe-iframe-resizer';
 
 // This fires when the parent of iframe resizes
 function onPymParentResize(width) {};
